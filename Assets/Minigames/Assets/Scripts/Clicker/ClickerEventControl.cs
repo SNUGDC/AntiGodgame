a8e4c7c34482e5457ee03855eceff129d1c5df82
Yang Gyeongmo <yangyeongmo@snu.ac.kr>
2018-08-14 21:56:16 +0900

    added two minigame(minesweeper, clicker)'s assets

---
+++ Assets/Minigames/Assets/Scripts/Clicker/ClickerEventControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickerEventControl : MonoBehaviour {
+
+    public float timerStart;
+    public GameObject ctrl;
+
+    private bool gameEnd = false;
+    private float timer;
+    private int count = 0;
+
+    private Text timerText;
+    private Text countText;
+
+
+	// Use this for initialization
+	void Start () {
+        timer = timerStart;
+        timerText = GameObject.Find("TimerText").GetComponent<Text>();
+        countText = GameObject.Find("CountText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerText.text = timer.ToString() + "초";
+        countText.text = "HITS: " + count.ToString();
+
+		if(timer > 0 && !gameEnd)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if(timer <= 0)
+        {
+            timer = 0;
+            gameEnd = true;
+        }
+        if (gameEnd)
+        {
+            OnGameEnd();
+        }
+	}
+
+    private void OnGameEnd()
+    {
+        //TODO: Do something on game end
+        //Deactivate Button
+        Button btn = GameObject.Find("CtrlButton").GetComponent<Button>();
+        btn.enabled = false;
+    }
+
+    public void ModifyCount(int mdfy)
+    {
+        print("count up by " + mdfy.ToString());
+        count += mdfy;
+    }
+}
---
+++ Assets/Minigames/Assets/Scripts/MineSweeper/EventControl.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventControl : MonoBehaviour {
+
+    public int maxMineCount, badBound, normalBound, goodBound;
+    public bool gameEnd = false;
+    public Text timerText;
+    public Text bugCountText;
+    public GameObject result;
+    public Image fade;
+
+    private Element[,] elements;
+    private Element elem;
+    private int mineCount = 0;
+    private int bugCount = 0;
+    private float timer = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+        elements = GridControl.elements;
+        if(mineCount != maxMineCount)
+        {
+            ModifyMineCount();
+        }
+	}
+
+    public void MineCountUp()
+    {
+        mineCount++;
+    }
+
+    public void MineCountDown()
+    {
+        mineCount--;
+    }
+
+    private void ModifyMineCount()
+    {
+        if (mineCount > maxMineCount)
+        {
+            elem = elements[Random.Range(0, GridControl.w), Random.Range(0, GridControl.h)];
+            while (!elem.mine && mineCount > 0)
+            {
+                elem = elements[Random.Range(0, GridControl.w), Random.Range(0, GridControl.h)];
+            }
+            elem.mine = false;
+            MineCountDown();
+            ModifyMineCount();
+        }
+        else if (mineCount < maxMineCount)
+        {
+            elem = elements[Random.Range(0, GridControl.w), Random.Range(0, GridControl.h)];
+            elem.mine = true;
+            MineCountUp();
+            ModifyMineCount();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timerText.text = timer.ToString() + "초";
+        bugCountText.text = "BUGS: " + bugCount.ToString() + "/" + maxMineCount.ToString();
+		if(timer > 0 && !gameEnd)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+            gameEnd = true;
+        }
+        if (gameEnd)
+        {
+            OnGameEnd();
+        }
+	}
+
+    private void OnGameEnd()
+    {
+        Text resultText = result.gameObject.GetComponentInChildren<Text>();
+        GridControl.UncoverMines();
+        if (bugCount > 0 && bugCount <= badBound)
+        {
+            resultText.text = "BAD";
+        }
+        else if (bugCount <= normalBound)
+        {
+            resultText.text = "NORMAL";
+        }
+        else if (bugCount <= goodBound)
+        {
+            resultText.text = "GOOD";
+        }
+        else if (bugCount <= maxMineCount)
+        {
+            resultText.text = "OVER";
+        }
+        fade.gameObject.SetActive(true);
+        result.gameObject.SetActive(true);
+    }
+
+    public void TimerModify(float time)
+    {
+        timer += time;
+    }
+
+    public float GetTimer()
+    {
+        return timer;
+    }
+
+    private void EndOfTimer()
+    {
+        print("Time Over!");
+    }
+
+    public void BugCountUp()
+    {
+        bugCount++;
+    }
+
+    public void BugCountDown()
+    {
+        bugCount--;
+    } 
+
+    public int GetBugCount()
+    {
+        return bugCount;
+    }
+}
