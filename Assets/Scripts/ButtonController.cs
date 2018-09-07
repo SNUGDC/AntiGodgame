75f33283127e497386347e9115bc4c85bd5fd26b
jayin301 <alswjdghk@snu.ac.kr>
2018-09-07 22:27:50 +0900

    implement SelectMiniGame scene

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+
+    public string nextScene;
+
+	public void StartNextDay()
+    {
+        int day = PlayerPrefs.GetInt("Day");
+        PlayerPrefs.SetInt("Day", day + 1);
+
+        SceneManager.LoadScene("StartDialogue");
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
